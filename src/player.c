202402f70a4f25a9d3dc403244189ba8560f606d
LananasBTW <159029997+LananasBTW@users.noreply.github.com>
2024-12-16 01:59:58 +0100

    Newton update

--- src/player.c
+++ src/player.c
@@ -1,42 +1,35 @@
 
 #include "../include/player.h"
 
-#define GRAVITY 9.81
-#define JUMP_INI  300
-#define TIME 1.0 / 30.0
-#define MASS 1.0 
+#define GRAVITY 981
+#define JUMP_FORCE  300
+#define TIME 1.0/30.0
 
 void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, myPlayer *player, myObject floor);
 
 
 void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, myPlayer *player, myObject floor) {
-    
+
     sfVector2f position = sfSprite_getPosition(player->object.sprite);
-    sfVector2f acceleration = {0, 0};   
 
-  
-    sfVector2f gravity = {0, GRAVITY_FORCE * MASS};
-    acceleration.y += gravity.y;
-
-    
-    if (sfKeyboard_isKeyPressed(sfKeyUp) && player->on_jump == 0) {
-        acceleration.y = -JUMP_FORCE / MASS;  
-        player->on_jump = 1;
-    }
-
-    player->velocity.y += acceleration.y * TIME;
-
-    // Met à jour la position en fonction de la vélocité
-    position.y += player->velocity.y * TIME;    
-
-    if (check_collision(player->object.sprite, floor.sprite)) {
-        position.y = floor_y_position - sprite_height;  
+    if(!check_collision(player->object.sprite, floor.sprite)) {
+        player->velocity.y += GRAVITY * TIME;
+    } else {
+        sfFloatRect player_bounds = sfSprite_getGlobalBounds(player->object.sprite);
+        sfFloatRect floor_bounds =  sfSprite_getGlobalBounds(floor.sprite);
+        position.y = floor_bounds.top - player_bounds.height;
         player->velocity.y = 0;
         player->on_jump = 0;
     }
 
+    if(sfKeyboard_isKeyPressed(sfKeyUp) && player->on_jump == 0) {
+        player->velocity.y = -JUMP_FORCE;
+        player->on_jump = 1;
+    }
 
-    // Gravité
+    position.y += player->velocity.y * TIME;
+
+     // Gravité
 
     // if (player->on_jump) {
     //     position.y -= player->speed.y;
