7a09270166c1156dadc197f9ea7ba2449f3d5b7d
LananasBTW <159029997+LananasBTW@users.noreply.github.com>
2024-12-16 16:28:03 +0100

    dash update

--- src/player.c
+++ src/player.c
@@ -4,15 +4,15 @@
 #define JUMP_FORCE 80
 #define TIME (1.0 / 32.0)
 #define DASH_FORCE 400
-#define DASH_DURATION 0.2*32
+#define DASH_DURATION 0.232
 
-void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, myPlayer *player, myObject floor) {
+void player_basics_movements(sfRenderWindow window, myWindowInfo window_info, myPlayer player, myObject floor) {
     // Récupère la position actuelle du joueur
     sfVector2f position = sfSprite_getPosition(player->object.sprite);
 
     // Applique la gravité si le joueur ne touche pas le sol
     if (!check_collision(player->object.sprite, floor.sprite)) {
-        player->velocity.y += GRAVITY * TIME;
+        player->velocity.y += GRAVITY TIME;
     } else {
         player->velocity.y = 0;
         player->on_jump = 0;
@@ -26,7 +26,7 @@ void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, m
     }
 
     // Gère le dash horizontal avec accélération et durée limitée
-    static float dash_timer = 0.0;  
+    static float dash_timer = 0.0;
 
     if (player->dash_ready && sfKeyboard_isKeyPressed(sfKeyLShift)) {
         if (sfKeyboard_isKeyPressed(sfKeyRight)) {
@@ -39,8 +39,7 @@ void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, m
             dash_timer = DASH_DURATION;
         }
     }
-
-    // Réduit le timer du dash
+// Réduit le timer du dash
     if (dash_timer > 0) {
         dash_timer -= TIME;
     } else {
@@ -55,6 +54,16 @@ void player_basics_movements(sfRenderWindow* window, myWindowInfo window_info, m
     } else if (player->velocity.x < 0) {
         player->velocity.x += 20 * TIME;
         if (player->velocity.x > 0) player->velocity.x = 0;
+    }
+
+    // Gère les déplacements horizontaux classiques (si pas en dash)
+    if (!player->is_dashing) {
+        if (sfKeyboard_isKeyPressed(sfKeyRight)) {
+            position.x += player->speed.x;
+        }
+        if (sfKeyboard_isKeyPressed(sfKeyLeft)) {
+            position.x -= player->speed.x;
+        }
     }
 
     // Applique les vitesses horizontale et verticale
