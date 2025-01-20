4437e334b52052fbb7d0c1b497ea92895e163330
LananasBTW <159029997+LananasBTW@users.noreply.github.com>
2025-01-20 23:02:00 +0100

    Settings add

--- include/settings.h
+++ include/settings.h
@@ -1,11 +1,9 @@
 #ifndef SETTINGS_H_
     #define SETTINGS_H_
 
-    #include "../include/my.h"
-    #include "../include/events.h"
-    #include "../include/sprites.h"
+    #include "my.h"
+    #include "events.h"
 
-    
     int settings_view(sfRenderWindow* window, sfEvent *event, myWindowInfo *window_info, int *program_step);
 
 #endif
